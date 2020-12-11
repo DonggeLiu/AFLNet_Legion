5a2c34bc23ec4ff4271bc9a0a2b85cc06404ee71
Thuan Pham <thuan.pham@unimelb.edu.au>
2020-12-11 11:30:33 +1100

    Cosmetic changes -- in progress

--- mcts.h
+++ mcts.h
@@ -31,7 +31,7 @@ typedef struct
 {
     // info
     gint id;
-    u32 * path;
+    u32 *path;
     u32 path_len;
 
     // statistics
@@ -39,82 +39,88 @@ typedef struct
     gint discovered;
 
     // input generation
-    void ** seeds;                  /* keeps all seeds reaching this node -- can be casted to struct seed_info_t* */
+    void **seeds; // keeps all seeds reaching this node -- can be casted to struct seed_info_t*
     int seeds_count;
-    TreeNode * simulation_child;
+    TreeNode *simulation_child;
 
     // property
     enum node_colour colour;
     gboolean fully_explored;
     gboolean exhausted;
-}TreeNodeData;
+} TreeNodeData;
 
 
 typedef struct
 {
-    void * q;
+    void *q; // Pointing to a specific queue entry/seed
     int selected;
     int discovered;
-}seed_info_t;
-
-
-/* Functions */
-/* ============================================== TreeNode Functions ============================================== */
-TreeNodeData * new_tree_node_data (int response_code, enum node_colour colour);
-
-TreeNode * new_tree_node(TreeNodeData * tree_data);
-
-TreeNodeData * get_tree_node_data(TreeNode * tree_node);
-
-TreeNode * get_simulation_child(TreeNode * tree_node);
-
-double tree_node_exploitation_score(TreeNode * tree_node);
-
-double tree_node_exploration_score(TreeNode * tree_node);
-
-double tree_node_score(TreeNode * tree_node);
-
-gboolean is_fully_explored(TreeNode *  tree_node);
-
-gboolean is_leaf(TreeNode * tree_node);
-
-gboolean fits_fish_bone_optimisation(TreeNode * tree_node);
-
-TreeNode * best_child(TreeNode * tree_node);
-
-char * mutate(TreeNode * tree_node);
-
-TreeNode * exists_child(TreeNode *  tree_node, int target_response_code);
-
-TreeNode * append_child(TreeNode * tree_node, int child_response_code, enum node_colour colour);
-
-void print_reversed_path(TreeNode * tree_node);
-u32 * collect_node_path(TreeNode * tree_node, u32 * path_len);
-void print_path(TreeNode * tree_node);
-u32 * collect_region_path(region_t region, u32 * path_len);
-
-int colour_encoder(enum node_colour colour);
-
-void tree_node_print (TreeNode * tree_node);
-
-seed_info_t * construct_seed_with_queue_entry(void * q);
-void add_seed_to_node(seed_info_t * seed, TreeNode * node);
-void find_M2_region(seed_info_t * seed, TreeNode * tree_node, u32 * M2_start_region_ID, u32 * M2_region_count);
-/* ============================================== TreeNode Functions ============================================== */
-/* ================================================ MCTS Functions ================================================ */
-TreeNode * select_tree_node(TreeNode * parent_tree_node);
-seed_info_t * select_seed(TreeNode * tree_node_selected);
-TreeNode * Initialisation();
-seed_info_t * Selection(TreeNode * parent_tree_node);
-char * Simulation(TreeNode * target);
-TreeNode * Expansion(TreeNode * tree_node, void *, u32 * response_codes, u32 len_codes, gboolean * is_new);
-void Propagation(TreeNode * leaf_selected, seed_info_t * seed_selected, gboolean is_new);
-
-/* ================================================ MCTS Functions ================================================ */
+} seed_info_t;
 
 /* Precomputation */
 //static TreeNode * ROOT = new_tree_node(new_tree_node_data(0,White,""));
 extern TreeNode * ROOT;
+
+
+/* Core functions */
+/* ============================================== TreeNode Functions ============================================== */
+TreeNodeData* new_tree_node_data(int response_code, enum node_colour colour);
+
+TreeNode* new_tree_node(TreeNodeData* tree_data);
+
+TreeNodeData* get_tree_node_data(TreeNode* tree_node);
+
+TreeNode* get_simulation_child(TreeNode* tree_node);
+
+double tree_node_exploitation_score(TreeNode* tree_node);
+
+double tree_node_exploration_score(TreeNode* tree_node);
+
+double tree_node_score(TreeNode* tree_node);
+
+gboolean is_fully_explored(TreeNode*  tree_node);
+
+gboolean is_leaf(TreeNode* tree_node);
+
+gboolean fits_fish_bone_optimisation(TreeNode* tree_node);
+
+TreeNode* best_child(TreeNode* tree_node);
+
+char* mutate(TreeNode* tree_node);
+
+TreeNode* exists_child(TreeNode* tree_node, int target_response_code);
+
+TreeNode* append_child(TreeNode* tree_node, int child_response_code, enum node_colour colour);
+
+/* ================================================ MCTS Functions ================================================ */
+TreeNode* select_tree_node(TreeNode* parent_tree_node);
+seed_info_t* select_seed(TreeNode* tree_node_selected);
+TreeNode* Initialisation();
+seed_info_t* Selection(TreeNode* parent_tree_node);
+char* Simulation(TreeNode* target);
+TreeNode* Expansion(TreeNode* tree_node, void*, u32* response_codes, u32 len_codes, gboolean* is_new);
+void Propagation(TreeNode* leaf_selected, seed_info_t* seed_selected, gboolean is_new);
+
+
+/* Helper functions */
+void print_reversed_path(TreeNode* tree_node);
+
+u32* collect_node_path(TreeNode* tree_node, u32* path_len);
+
+void print_path(TreeNode* tree_node);
+
+u32* collect_region_path(region_t region, u32* path_len);
+
+int colour_encoder(enum node_colour colour);
+
+void tree_node_print (TreeNode* tree_node);
+
+seed_info_t* construct_seed_with_queue_entry(void* q);
+
+void add_seed_to_node(seed_info_t* seed, TreeNode* node);
+
+void find_M2_region(seed_info_t* seed, TreeNode* tree_node, u32* M2_start_region_ID, u32* M2_region_count);
+
 
 #endif //MCTS_H
 
