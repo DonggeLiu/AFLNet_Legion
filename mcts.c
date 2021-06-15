67e8cb3e0c9959cf96166fe52020634071dba3f5
DonggeLiu <alan32.liu@gmail.com>
2021-06-15 13:27:42 +1000

    Revert "log seed count of each node"
    This reverts commit e8694c4f1854aff8398f7122a231d502aaa2848f.

--- mcts.c
+++ mcts.c
@@ -469,18 +469,14 @@ char* tree_node_repr(TreeNode* tree_node)
   TreeNodeData* tree_node_data = get_tree_node_data(tree_node);
   char* message = NULL;
 
-  message_append(&message, "\033[1;%dm%03u: %06.2lf "
-                           "[%06.2lf + %06.2lf] "
-                           "{%03u, %03u} "
-                           "<%03u>\033[0m",
+  message_append(&message, "\033[1;%dm %03u: %06.2lf [%06.2lf + %06.2lf] {%03u, %03u}\033[0m",
            colour_encoder(tree_node_data->colour),
            tree_node_data->id,
            tree_node_score(tree_node),
            tree_node_exploitation_score(tree_node),
            tree_node_exploration_score(tree_node),
            tree_node_data->selected,
-           tree_node_data->discovered,
-           tree_node_data->seeds_count);
+           tree_node_data->discovered);
   return message;
 }
 
